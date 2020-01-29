9a2c4ce3116452d378f542d038251c301a8701df
brian-noh <brian-noh@users.noreply.github.com>
2020-01-28 21:25:20 -0500

    Created edit, delete, create and added User Model

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -4,11 +4,11 @@
     let users = [];
 
 
-    var $usernameFld = $("#usernameFld");
+    var $usernameFld;
     var $passwordFld = $("#passwordFld");
     var $removeBtn = $("wbdv-remove");
     var $editBtn = $("wbdv-edit");
-    var $createBtn = $("wbdv-create");
+    var $createBtn;
     var $firstNameFld = $("#firstnameFld");
     var $lastNameFld = $("#lastNameFld");
     var $roleFld= $("#roleFld");
@@ -18,12 +18,80 @@
     $(main);
 
 
-    function createUser() {}
-    function findAllUsers() {}
-    function findUserById() {}
-    function deleteUser() {}
+    function createUser() {
+        const newUser = {
+            username: $usernameFld.val(),
+            password: $passwordFld.val(),
+            firstName: $firstNameFld.val(),
+            lastName: $lastNameFld.val(),
+            role: $roleFld.val()
+
+        }
+        $usernameFld.val("");
+        $passwordFld.val("");
+        $firstNameFld.val("");
+        $lastNameFld.val("");
+        $roleFld.val("");
+
+
+        userService.createUser(newUser)
+            .then((actualUser) => {
+                 users.push(actualUser)
+                 renderUsers()
+                findAllUsers()
+            })
+    }
+    function findAllUsers() {
+        userService
+            .findAllUsers()
+            .then(theusers => {
+                users = theusers
+                renderUsers()
+            })
+    }
+    function findUserById(userId) {
+        userService
+            .findUserById(userId)
+            .then(theusers => {
+                users = theusers
+                renderUsers()
+            })
+    }
+    function deleteUser(index) {
+        let user = users[index]
+        let userId = user._id
+
+        userService.deleteUser(userId)
+            .then(response => {
+                users.splice(index, 1)
+                renderUsers()
+            })
+    }
     function selectUser() {}
-    function updateUser() {}
+
+
+
+    function updateUser() {
+        const updatedUser = {
+            username: $usernameFld.val(),
+            password: $passwordFld.val(),
+            firstName: $firstNameFld.val(),
+            lastName: $lastNameFld.val(),
+            role: $roleFld.val()
+        }
+        $usernameFld.val("")
+        $passwordFld.val("");
+        $firstNameFld.val("");
+        $lastNameFld.val("");
+        $roleFld.val("");
+
+        updatedUser._id = users[currentUserIndex]._id
+
+        userService.updateUser(updatedUser._id, updatedUser)
+            .then((actualUser) => {
+                findAllUsers()
+            })
+    }
     function renderUser(user) {}
     function renderUsers() {
         $tbody.empty()
@@ -31,7 +99,11 @@
             let user = users[u];
             // $remove.click(() => deleteUser(u))
 
-            // $editBtn.click(() => updateUser(u))
+            $removeBtn = $("<button> Delete</button>")
+            $removeBtn.click(() => deleteUser(u))
+
+            $editBtn = $("<button> Edit</button>")
+            $editBtn.click(() => updateUser(u))
 
 
             const rowClone = $userRowTemplate.clone();
@@ -43,10 +115,21 @@
             rowClone.find('.wbdv-role').html(user.role);
 
             $tbody.append(rowClone);
+
+            $tbody.append($removeBtn);
+            $tbody.append($editBtn);
+
         }
     }
 
     function main() {
+
+        $usernameFld = $("#usernameFld");
+        $createBtn = $("#createBtn");
+
+        $createBtn.click(createUser)
+        //$updateBtn.click(updateUser)
+
         userService
             .findAllUsers()
             .then(theusers => {
